b2ed2c56a8b9d135e47ed81acd706ba4b13b3423
K0st3k6969 <p.romanczuk@gmail.com>
2022-11-12 21:27:45 +0100

    added first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+	return (
+		<div>
+			<h2>March 28th 2022</h2>
+			<div>
+				<h3>Car insurane</h3>
+                <div>$294.41</div>
+			</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
