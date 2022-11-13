a08efe478d96568b5d390db24488945aabf64778
K0st3k6969 <p.romanczuk@gmail.com>
2022-11-13 20:35:26 +0100

    exporting ExpenseDate to the another comoponent + adding CSS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
@@ -8,16 +9,17 @@ function ExpenseItem(props) {
 	// const expenseTitle = 'Car Insurance';
 	// const expenseAmount = 397.45;
 
-	return (
-		<div className="expense-item">
-			
-			{/* Function Date is an object not a string, 
-			so it had to be strigify by method".toISOString()*/}
 
-			<div>{props.date.toISOString()}</div>
+	return (
+
+			{/* Function Date is an object not a string, 
+			so it had to be strigify by method".toISOString()*/},
+			
+		<div className="expense-item">
+			<ExpenseDate date={props.date}/>
 			<div className='expense-item__description'>
 				<h3>{props.title}</h3>
-                <div className='expense-item__price'>${props.amount}</div>
+				<div className='expense-item__price'>${props.amount}</div>
 			</div>
 		</div> 
 	);
